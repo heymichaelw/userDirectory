d29732f130e513f6f61c356321d78f32814afa46
Michael <heymichaelw@gmail.com>
2017-07-10 12:51:01 -0400

    created details page

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const mustacheExpress = require('mustache-express');
 const path = require('path');
 const app = express();
 const directory = require('./36d3402e-data.js');
+const filteredList = [];
 
 app.use(express.static('public'));
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.get('/directory', function(req, res){
+app.get('/', function(req, res){
   res.render('directory', directory);
+});
+
+app.get('/:id', function(req, res){
+  function findOne(item){
+    return item.id === Number(req.params.id);
+  }
+  var filteredList = directory.users.find(findOne);
+  var context = {model: filteredList};
+  res.render('details', context);
 });
 
 
