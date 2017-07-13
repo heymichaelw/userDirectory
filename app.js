cdbafc20eb99b5e3500ccad3bba54eb1e11af7c7
Michael <heymichaelw@gmail.com>
2017-07-13 11:25:57 -0400

    heroku deployment

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get('/:id', function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('Express app listening for connections');
 });
